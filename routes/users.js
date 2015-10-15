9c697546f471ee4a1cdff0f38aec423df032d4aa
shyb0904@163.com <shyb0904@163.com>
2015-10-16 01:03:32 +0800

    test

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,4 @@
+'user strict';
 var express = require('express');
 var router = express.Router();
 
@@ -6,8 +7,15 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/list', function(req, res, next){
-	res.render();
+router.get('/list', function(req, res, next) {
+  res.render('userlist', {userList: []});
 });
+
+var array = [];
+array.push("array");
+array.push("byte");
+for(var i = 0; i < array.length; i++) {
+    console.console.log(array[i]);
+}
 
 module.exports = router;
