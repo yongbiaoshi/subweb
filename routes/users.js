f28e9b8e8afe7dff44aa8d3a6e29834da4897425
yongbiaoshi <shyb0904@163.com>
2015-10-16 11:12:57 +0800

    add something

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,17 @@
-'use strict';
+/**
+ * 用户Controller
+ */
+
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/**
+ *  GET users listing.
+ * @param  {[request]} req   [请求]
+ * @param  {[response]} res   [响应]
+ * @param  {[function]} next) {             res.send('respond with a resource');} [返回用户列表]
+ * @return {[json]}       [返回user listing]
+ */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
@@ -15,6 +24,7 @@ var array = [];
 array.push("array");
 array.push("byte");
 array.push("string");
+
 for(var i = 0; i < array.length; i++) {
     console.log(array[i]);
 }
