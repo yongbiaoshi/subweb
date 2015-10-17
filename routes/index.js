e7bade54fc8a5ce5f2dd34058ce4feb379dcf0b0
shyb0904@163.com <shyb0904@163.com>
2015-10-18 02:48:36 +0800

    继续学习

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {title: "系统之家-网站首页"});
 });
 
 module.exports = router;
