4c778410a16e22216b81cd95582ec9a26122f6dc
shyb0904@163.com <shyb0904@163.com>
2015-10-18 23:15:14 +0800

    add something

--- routes/users.js
+++ routes/users.js
@@ -16,8 +16,17 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/**
+ *  GET users listing.
+ * @param  {[request]} req   [请求]
+ * @param  {[response]} res   [响应]
+ * @param  {[function]} next) [返回用户列表]
+ * @return {[json]}       [返回user listing]
+ */
 router.get('/list', function(req, res, next) {
-  res.render('userlist', {users: [{"name": "wlb"}, {"name": "fyj"}, {"name": "hff"}]});
+  var users = [];
+  users.push({"name": "wlb"}, {"name": "fyj"}, {"name": "hff"}, {"name": "xb"});
+  res.render('userlist', {users: users});
 });
 
 module.exports = router;
