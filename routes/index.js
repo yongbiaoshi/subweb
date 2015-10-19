020699419b809a5b4bd21ef83871e63a833ab6d2
yongbiaoshi <shyb0904@163.com>
2015-10-19 18:29:23 +0800

    go on

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var moment = require('moment');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {title: "系统之家-网站首页"});
+  var result = {};
+  result.title = "系统之家-网站首页";
+  // result.now = moment(new Date()).format('llll');//当前日期格式化
+  result.now = moment(new Date()).format('YYYY-MM-D日');//当前日期格式化
+  res.render('index', result);
 });
 
 module.exports = router;
